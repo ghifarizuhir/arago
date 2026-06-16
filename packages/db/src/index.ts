7a188fcec6fc8f87eed46587d22d6f0a437d092f
CTO (KarsaLabs) <cto@karsalabs.com>
2026-06-16 14:13:48 +0700

    chore: remove prior scaffold to rebuild from Phase 1 slice plans

--- packages/db/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./schema";
-export { db } from "./client";
-export type { Database } from "./client";
--- packages/test-utils/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { seedTestDistrict, seedTestSchool, seedTestUser, seedTestClass, seedTestDatabase, cleanupTestData } from "./factories";
-export { TEST_CONSTANTS } from "./fixtures";
